e7cda0819ee5226e67aa4157a26cc1589d09c0b1
nikol <test2@yahoo.com>
2019-03-26 11:51:40 +0430

    creat model DataBase in models.py

--- ElectionManager/models.py
+++ ElectionManager/models.py
@@ -1,3 +1,14 @@
 from django.db import models
 
 # Create your models here.
+
+class Election_db(models.Model):
+ id = models.AutoField(primary_key=True)
+ Title=models.CharField(max_length=30,null=False,blank=True)
+ Start_Time=models.DateField(null=True,blank=True)
+ End_Time=models.DateField(null=True,blank=True)
+ List_Of_Choices=models.CharField(max_length=500,null=False,blank=True)
+ Number_Of_Votes=models.IntegerField(null=True,blank=True)
+
+ def __str__(self):
+  return str(self.Title)
