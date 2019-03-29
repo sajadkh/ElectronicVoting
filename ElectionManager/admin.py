84e8a162c60852be136af337c0bd9f12f0286af0
sajadkh <sajadkhosravi1996@gmail.com>
2019-03-29 13:28:08 +0430

    review models and edit them

--- ElectionManager/admin.py
+++ ElectionManager/admin.py
@@ -1,4 +1,4 @@
 from django.contrib import admin
-from .models import Election_db
+from .models import Election
 # Register your models here.
-admin.site.register(Election_db),
+admin.site.register(Election),
--- ElectionManager/models.py
+++ ElectionManager/models.py
@@ -1,14 +1,15 @@
 from django.db import models
 
+
 # Create your models here.
 
-class Election_db(models.Model):
- id = models.AutoField(primary_key=True)
- Title=models.CharField(max_length=30,null=False,blank=True)
- Start_Time=models.DateField(null=True,blank=True)
- End_Time=models.DateField(null=True,blank=True)
- List_Of_Choices=models.CharField(max_length=500,null=False,blank=True)
- Number_Of_Votes=models.IntegerField(null=True,blank=True)
+class Election(models.Model):
+    id = models.AutoField(primary_key=True)
+    Title = models.CharField(max_length=30)
+    Start_Time = models.DateField()
+    End_Time = models.DateField()
+    List_Of_Choices = models.CharField(max_length=1000)
+    Number_Of_Votes = models.IntegerField(default=0)
 
- def __str__(self):
-  return str(self.Title)
+    def __str__(self):
+        return str(self.Title)
