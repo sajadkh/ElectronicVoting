777c16cad69a2113248dc11419355b6a3aa1a5ce
nikol <test2@yahoo.com>
2019-04-08 19:55:26 +0430

    make new table fo list choice - fekr mikonam dg kar kone test kon

--- ElectionManager/models.py
+++ ElectionManager/models.py
@@ -8,8 +8,20 @@ class Election(models.Model):
     Title = models.CharField(max_length=30)
     Start_Time = models.DateField()
     End_Time = models.DateField()
-    List_Of_Choices = models.CharField(max_length=1000)
+   # List_Of_Choices = models.CharField(max_length=1000)
     Number_Of_Votes = models.IntegerField(default=0)
 
     def __str__(self):
         return str(self.Title)
+
+
+
+class ListOfChoicese(models.Model):
+ id = models.AutoField(primary_key=True)
+ Choices=models.CharField(max_length=1000)
+ foreigkey=models.ForeignKey(Election, on_delete=models.CASCADE)
+ 
+
+
+
+
