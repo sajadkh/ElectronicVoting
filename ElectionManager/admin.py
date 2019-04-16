679b4a3e742c3409d0e1e55e2ba625ab2b6c1bf3
kosravi <sajadkhosravi1996@gmail.com>
2019-04-16 21:39:16 +0430

    add listOfChoices model to admin panel

--- ElectionManager/admin.py
+++ ElectionManager/admin.py
@@ -1,4 +1,5 @@
 from django.contrib import admin
-from .models import Election
+from .models import *
 # Register your models here.
 admin.site.register(Election),
+admin.site.register(ListOfChoicese),
