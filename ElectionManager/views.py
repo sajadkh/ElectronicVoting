2311b39f6487bbc1e39224a9d851cd1c5e0339b2
kosravi <sajadkhosravi1996@gmail.com>
2019-04-17 21:30:35 +0430

    add view for get list of elections and details of election

--- ElectionManager/views.py
+++ ElectionManager/views.py
@@ -17,16 +17,46 @@ class ElectionView(APIView):
         if election_serializer.is_valid(raise_exception=True):
             election_data_saved = election_serializer.save()
             list_of_choices = request.data.get('ListOfChoices')
-            for i in range(0,2):
+            for choice in list_of_choices:
                 list_of_choices_data = {
-                    'Title': list_of_choices[i],
+                    'Title': choice,
                     'Election_ref': election_data_saved.id
                 }
                 list_of_choices_serializer = ListOfChoicesSerializer(data=list_of_choices_data)
                 if list_of_choices_serializer.is_valid(raise_exception=True):
                     list_of_choices_saved = list_of_choices_serializer.save()
-                    return Response({}, status=status.HTTP_200_OK)
                 else:
                     return Response({}, status=status.HTTP_500_INTERNAL_SERVER_ERROR)
+            return Response({}, status=status.HTTP_200_OK)
         else:
+            return Response({}, status=status.HTTP_500_INTERNAL_SERVER_ERROR)
+
+    def get(self, request, format=None):
+        try:
+            id = request.query_params.get('id')
+            if id:
+                try:
+                    elections = Election.objects.get(id=id)
+                except Election.DoesNotExist:
+                    return Response({}, status=status.HTTP_404_NOT_FOUND)
+                data = {}
+                for e in elections:
+                    data = {
+                        'id': e.id,
+                        'Title': e.Title,
+                        'Start_Time': e.Start_Time,
+                        'End_Time': e.End_Time,
+                        'Number_Of_Votes': e.Number_Of_Votes
+                    }
+                return Response(data, status=status.HTTP_200_OK)
+            else:
+                elections = Election.objects.all()
+                data = []
+                for e in elections:
+                    data.append({
+                        'id': e.id,
+                        'Title': e.Title
+                    })
+                return Response(data, status=status.HTTP_200_OK)
+        except:
             return Response({}, status=status.HTTP_500_INTERNAL_SERVER_ERROR)
